19648f7789f03473159f05045a9c863776551e47
William Edwin Sharkey <williamsharkey@gmail.com>
2021-02-20 10:34:05 -0500

    test a grammar

---
+++ main.go
@@ -0,0 +1,41 @@
+package peg
+
+import "github.com/yhirose/go-peg"
+
+func TestParser(grammar string, tests ...string) (results []string) {
+
+	formulaParser, err := peg.NewParser(grammar)
+	if err != nil {
+		results = append(results, err.Error())
+		return
+	}
+
+	for _, t := range tests {
+		ast, err := formulaParser.ParseAndGetAst(t, nil)
+		if err != nil {
+			results = append(results, err.Error())
+		} else {
+			results = append(results, ast.String())
+		}
+	}
+	return
+}
+
+func Example() {
+	grammar := `
+    # Simple calculator
+    EXPR         ←  ATOM (BINOP ATOM)*
+    ATOM         ←  NUMBER / ('(' EXPR ')') / ('"' TEXT '"')
+    BINOP        ←  < [-+/*&] >
+    NUMBER       ←  < [0-9]+ >
+	TEXT         ←  < [A-Za-Z]+ >
+    %whitespace  ←  [ \t]*
+    ---
+    # Expression parsing option
+    %expr  = EXPR   # Rule to apply 'precedence climbing method' to
+    %binop = L + -  # Precedence level 1
+    %binop = L * /  # Precedence level 2
+	%binop = L &    # Precedence level 3
+`
+	TestParser(grammar, `"hello"&world`, `"hello"&"cool"&"world"`, `"hi "&"world"`)
+}
