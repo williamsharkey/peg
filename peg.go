5ceeb648f899239e2c1b1ef3338f495c03107dd8
William Edwin Sharkey <williamsharkey@gmail.com>
2021-02-23 11:55:05 -0500

    enable ast

--- peg.go
+++ peg.go
@@ -1,4 +1,4 @@
-package peg
+package main
 
 import (
 	"fmt"
@@ -10,14 +10,38 @@ func TestParser(grammar string, test string) (ast string) {
 
 	tests := strings.Split(test, "~")
 	results := []string{}
-	formulaParser, err := peg.NewParser(grammar)
+	parser, err := peg.NewParser(grammar)
 	if err != nil {
 		results = append(results, err.Error())
 		return strings.Join(results, "~")
 	}
 
+	parser.EnableAst()
+	//g := parser.Grammar
+	//g["EXPR"].Action = func(v *peg.Values, d peg.Any) (peg.Any, error) {
+	//	val := v.ToInt(0)
+	//	if v.Len() > 1 {
+	//		ope := v.ToStr(1)
+	//		rhs := v.ToInt(2)
+	//		switch ope {
+	//		case "+": val += rhs
+	//		case "-": val -= rhs
+	//		case "*": val *= rhs
+	//		case "/": val /= rhs
+	//		}
+	//	}
+	//	return val, nil
+	//}
+	//g["BINOP"].Action = func(v *peg.Values, d peg.Any) (peg.Any, error) {
+	//	return v.Token(), nil
+	//}
+	//g["NUMBER"].Action = func(v *peg.Values, d peg.Any) (peg.Any, error) {
+	//	return strconv.Atoi(v.Token())
+	//}
+
 	for _, t := range tests {
-		s, errP := formulaParser.ParseAndGetValue(t, nil)
+		s, errP := parser.ParseAndGetAst(t, nil)
+		//s, errP := formulaParser.ParseAndGetValue(t, nil)
 		if errP != nil {
 			results = append(results, errP.Error())
 		} else {
@@ -39,23 +63,24 @@ func Example() {
 
 func GrammarExample() string {
 	return `
-# Add Comment
-# Simple calculator
-EXPR         ←  ATOM (BINOP ATOM)*
-ATOM         ←  NUMBER / ('(' EXPR ')') / ('"' TEXT '"')
-BINOP        ←  < [-+/*&] >
-NUMBER       ←  < [0-9]+ >
-TEXT         ←  < [A-Za-Z]+ >
-%whitespace  ←  [ \t]*
----
-# Expression parsing option
-%expr  = EXPR   # Rule to apply 'precedence climbing method' to
-%binop = L + -  # Precedence level 1
-%binop = L * /  # Precedence level 2
-%binop = L &    # Precedence level 3
+    # Simple calculator
+    EXPR         ←  ATOM (BINOP ATOM)*
+    ATOM         ←  NUMBER / '(' EXPR ')'
+    BINOP        ←  < [-+/*] >
+    NUMBER       ←  < [0-9]+ >
+    %whitespace  ←  [ \t]*
+    ---
+    # Expression parsing option
+    %expr  = EXPR   # Rule to apply 'precedence climbing method' to
+    %binop = L + -  # Precedence level 1
+    %binop = L * /  # Precedence level 2
 `
 }
 
 func TestExample() string {
-	return `1~"hello"&world~"hello"&"cool"&"world"~"hi "&"world"`
+	return `1+1`
+}
+
+func main() {
+	Example()
 }
