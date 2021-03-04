81b67c5f47875084b0f39fed11ae9fb1ab45e2bf
William Edwin Sharkey <williamsharkey@gmail.com>
2021-03-04 12:06:05 -0500

    use williamsharkey/go-peg fork use :note: instead of # for indicating comments verify that binary operator named #OR# works

--- peg.go
+++ peg.go
@@ -2,12 +2,12 @@ package peg
 
 import (
 	"fmt"
-	"github.com/yhirose/go-peg"
+	"github.com/williamsharkey/go-peg"
 	"strings"
 )
 
 func TestParser(grammar string, test string) (results string) {
-
+	peg.CommentCharacterSet(":note:")
 	parser, err := peg.NewParser(grammar)
 	if err != nil {
 		return "Grammar Parse Error: " + err.Error()
@@ -28,20 +28,21 @@ func TestParser(grammar string, test string) (results string) {
 func GrammarExample() string {
 	return `
 
-# Grammar with strings
+:note: Grammar with strings
 EXPR         ←  ATOM (BINOP ATOM)*
 ATOM         ←  NUMBER / STRING / ATFN / REF / '(' EXPR ')'
 ATFN         ←  '@' <[A-Za-z]*> '(' <ATOM> ( ',' <ATOM>)*  ')' 
-BINOP        ←  < [-+/*&] >
+BINOP        ←  < [-+/*&] / '#OR#' >
 NUMBER       ←  < [0-9]+ >
 REF          ←  < (!(BINOP/'"'/ [ \t]).)+ >
 STRING       ←  ["] < (!('"')./'""')*  > ["] [ \t]* 
 %whitespace  ←  [ \t]*
 ---
-# Expression parsing option
-%expr  = EXPR   # Rule to apply 'precedence climbing method' to
-%binop = L + - &  # Precedence level 1
-%binop = L * /  # Precedence level 2
+:note: Expression parsing option
+%expr  = EXPR     :note: Rule to apply 'precedence climbing method' to
+%binop = L #OR#   :note: Precedence level 1
+%binop = L + - &  :note: Precedence level 2
+%binop = L * /    :note: Precedence level 3
 
 `
 }
@@ -49,5 +50,6 @@ STRING       ←  ["] < (!('"')./'""')*  > ["] [ \t]*
 func TestExample() string {
 	return `1+1
 hello
-"hello world"&"abc"`
+"hello world"&"abc"
+"a"#OR#"b"`
 }
